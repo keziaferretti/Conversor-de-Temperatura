06b4be16fd4bf0665cb1f3b7f68b4ecf0e8f71ba
Kézia Ferretti <99621696+keziaferretti@users.noreply.github.com>
2022-09-13 22:07:23 -0300

    Add files via upload

---
+++ calculadora-de-graus-celsius-em-fahrenheit/README.md
@@ -0,0 +1,4 @@
+# Calculadora de graus Celsius em Fahrenheit
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/keziaferretti/pen/YzLpqoV](https://codepen.io/keziaferretti/pen/YzLpqoV).
+
---
+++ calculadora-de-graus-celsius-em-fahrenheit/dist/script.js
@@ -0,0 +1,8 @@
+function converter() {
+  var conversao = parseInt(document.getElementById("celsius").value);
+  var convertido = (conversao * 9) / 5 + 32;
+  var elementoResultado = document.getElementById("resultado");
+  
+ var resultado = "A temperatura é de: " + convertido;elementoResultado.innerHTML = resultado + "F°";
+  
+}
